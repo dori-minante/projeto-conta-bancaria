f09c64d8129e34d6ae4bd62b8856f920f8c11ea4
Dorivania Santos <dorim.dev@gmail.com>
2024-02-19 15:11:02 -0300

    first commit

---
+++ src/conta/Menu.java
@@ -0,0 +1,17 @@
+package conta;
+
+import conta.model.Conta;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		Conta c1 = new Conta(181309, 452, 1, "Bianca Santos Ribeiro", 18000.0f);
+		
+		c1.visualizar();
+		
+
+		
+	}
+
+}
