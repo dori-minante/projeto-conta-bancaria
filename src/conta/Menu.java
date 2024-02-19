dda619da929356b9b3f35317fd58c6db461dcff2
Dorivania Santos <dorim.dev@gmail.com>
2024-02-19 19:56:25 -0300

    adicionando gitignore

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -6,11 +6,19 @@ public class Menu {
 
 	public static void main(String[] args) {
 		
-		Conta c1 = new Conta(181309, 452, 1, "Bianca Santos Ribeiro", 18000.0f);
+		Conta c1 = new Conta(1, 123, 1, "Sabrina", 250000.0f);
 		
 		c1.visualizar();
+		c1.setAgencia(456);
+		System.out.println(c1.getAgencia());
 		
-
+		if (c1.sacar(100))
+			System.out.println("Saque efetuado com sucesso. O novo saldo é de: " + c1.getSaldo());
+		else
+			System.out.println("O saldo é insuficiente!");
+		
+		c1.depositar(1000);
+		System.out.println("Deposito efetuado com sucesso. O novo saldo é de: " + c1.getSaldo());
 		
 	}
 
