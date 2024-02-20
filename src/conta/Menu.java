ef6707a7d593263fcc824417611bd0d9027620a0
Dorivania Santos <dorim.dev@gmail.com>
2024-02-20 20:58:02 -0300

    Adicionado branch task01

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,17 +1,10 @@
 package conta;
 
-import conta.model.Conta;
-
 public class Menu {
 
 	public static void main(String[] args) {
 		
-		Conta c1 = new Conta(181309, 452, 1, "Bianca Santos Ribeiro", 18000.0f);
-		
-		c1.visualizar();
-		
-
-		
+				
 	}
 
 }
--- src/conta/model/Conta.java
+++ src/conta/model/Conta.java
@@ -1,7 +1,7 @@
 package conta.model;
 
 public class Conta {
-
+	
 	private int numero;
 	private int agencia;
 	private int tipo;
